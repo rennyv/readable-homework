7d2ded5454a81c5b20f608194269f0198ec77f83
Renny Vanneste <renny@vanneste.ca>
2017-09-11 22:54:58 -0600

    Added bootstrap and frame for rest of the stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import * as api from '../utils/api.js'
 import Post from './Posts'
+import { Grid, Row, Col, Panel } from 'react-bootstrap'
 
 class App extends Component {
   state = {
@@ -18,12 +19,28 @@ class App extends Component {
     const { posts } = this.state
 
     return (
-      <div className="App">
-         {posts.map((post) => (
-           <Post post={post} />
-         ))}
-      </div>
-    );
+      <Grid>
+        <Row>
+          
+        </Row>
+        <Row>
+          <Col xs={12} md={8}>
+            <h4>
+              Posts:
+            </h4>
+            {posts.map((post) => (
+              <Panel key={post.id} header={ post.title }>
+                <Post post={post} />
+              </Panel>
+            ))}
+          </Col>
+          <Col xs={6} md={4}>
+            <h4>Sort by:</h4>
+            <h4>Catagories:</h4>
+          </Col>
+        </Row>
+      </Grid>
+    )
   }
 }
 
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -7,8 +7,7 @@ class Post extends Component {
        
     return (
       <div className="post">
-        <div className="id">{ post.id }</div>
-        <div className="title">{ post.title }</div>
+        <div className="body">{ post.body }</div>
       </div>
     )
   }
