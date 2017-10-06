2cc5c64a67d57bd38f14667493ce1a9618024ddb
Renny Vanneste <renny@vanneste.ca>
2017-10-05 22:04:54 -0600

    exporting postorder now

--- frontend/src/reducers/postsOrder.js
+++ frontend/src/reducers/postsOrder.js
@@ -2,7 +2,7 @@ import * as actions from '../actions'
 
 const initialPostSort = "author" 
 
-function postsOrder (state = initialPostSort, action ){
+export function postsOrder (state = initialPostSort, action ){
   switch (action.type){
     case actions.CHANGE_POST_ORDER:
       return action.order
